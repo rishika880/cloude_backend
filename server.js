9cf2b6a540c58090439664e39490930ce0875d4c
Rishika <rishika880@gmail.com>
2026-06-28 12:18:39 +0530

    added new crud

--- server.js
+++ server.js
@@ -9,10 +9,39 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.send('it is about page')
 })
-app.get('/user',async(req,res)=>{
+app.get('/users',async(req,res)=>{
     const result= await pool.query('SELECT * FROM users')
     res.json(result.rows)
 })
+app.post('/users', async (req, res) => {
+  const { name, age, college } = req.body
+  const result = await pool.query(
+    'INSERT INTO users (name, age, college) VALUES ($1, $2, $3) RETURNING *',
+    [name, age, college]
+  )
+  res.json(result.rows[0])
+})
+
+// PUT - update a user
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, age, college } = req.body
+  const result = await pool.query(
+    'UPDATE users SET name=$1, age=$2, college=$3 WHERE id=$4 RETURNING *',
+    [name, age, college, id]
+  )
+  res.json(result.rows[0])
+})
+
+// DELETE - remove a user
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params
+  await pool.query('DELETE FROM users WHERE id=$1', [id])
+  res.json({ message: 'user deleted successfully' })
+})
+
+
+
 app.listen(3000,()=>{
     console.log('server is running in the localaddress 3000')
 })
